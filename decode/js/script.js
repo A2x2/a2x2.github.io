0fe13929e896a30450ff18e5e0e359eb515f2889
A4 <automaticfour@gmail.com>
2016-12-16 23:48:51 +0800

    New arrow

--- decode/js/script.js
+++ decode/js/script.js
@@ -22,7 +22,7 @@ function xlate() {
 			const r = window[id](value);
 			if (r) {
 				const inf = Data[id];
-				resultsHTML += `<section class="result-section" style="border-left-color: ${inf.colour}"><h3 style="color: ${inf.colour};">${inf.name}</h3><h5> 🡒 ${inf.type}</h5><p></p>${r}</section>`;
+				resultsHTML += `<section class="result-section" style="border-left-color: ${inf.colour}"><h3 style="color: ${inf.colour};">${inf.name}</h3><h5> → ${inf.type}</h5><p></p>${r}</section>`;
 			}
 		}
 	}
